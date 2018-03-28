f31e3c24501deff04951aec4457ba09332a73978
scouttea <sam.coutteau@ugent.be>
2018-03-28 11:11:27 +0200

    added iron resource rocks

---
+++ graphics/decorative/iron-rock/iron-rock.lua
@@ -0,0 +1,32 @@
+local iron_rock = {}
+
+iron_rock.pictures =
+    {
+      { filename = "__Caverns__/graphics/decorative/iron-rock/iron-rock-01.png",
+        width = 76,
+        height = 60,
+        shift = {0.1, 0} 
+      },
+      { filename = "__Caverns__/graphics/decorative/iron-rock/iron-rock-02.png",
+        width = 83,
+        height = 86,
+        shift = {0.2, 0}
+      },
+      { filename = "__Caverns__/graphics/decorative/iron-rock/iron-rock-03.png",
+        width = 126,
+        height = 98,
+        shift = {0.7, 0}
+      },
+      { filename = "__Caverns__/graphics/decorative/iron-rock/iron-rock-04.png",
+        width = 92,
+        height = 108,
+        shift = {0.1, 0}
+      },
+      { filename = "__Caverns__/graphics/decorative/iron-rock/iron-rock-05.png",
+        width = 140,
+        height = 99,
+        shift = {0.5, 0}
+      }
+    }
+    
+return iron_rock
